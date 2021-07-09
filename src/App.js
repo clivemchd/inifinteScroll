567eedfcdfdaf2599ab37fc4ab2a4e06013bcfd1
Clive Machado <clive.machado@raweng.com>
2021-07-09 18:09:37 +0530

    Big Bang

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+/* eslint-disable no-unreachable */
 import './App.css';
+import { fetchAPI, realFetchAPI } from './fetchAPI';
+import { useEffect, useState, useRef } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const InfiniteScrollingBitch = () => {
+
+	const [itemList, setItemList] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [page, setPage] = useState(0);
+	const loaderRef = useRef(null);
+	
+	const handleUpdate = (entities, observer) => {
+		const newPage = page + 1;
+		console.log("test ", page, newPage);
+		getData(newPage);
+		setPage(newPage);
+	}
+	
+	const getData = (page) => {
+		setLoading(true);
+		realFetchAPI(page, 10)
+		.then((response) => {
+			setItemList(() => { return [...itemList, ...response]});
+			setLoading(false);
+		})
+	} 
+	
+	useEffect(() => {
+		getData(page);
+
+		// let options = {
+		// 	root: null,
+		// 	rootMargin: '0px',
+		// 	threshold: 0.1
+		// }
+		
+		// let observer = new IntersectionObserver(handleUpdate, options);
+		// observer.observe(loaderRef.current);
+	}, []);
+
+	const loadingCSS = {
+		height: "100px",
+		margin: "30px"
+	};
+
+	const loadingTextCSS = {
+		border: `1px solid yellow`,  
+		display: loading ? "block" : "none"
+	};
+	
+	return (
+		<div>
+			<div id="scrollArea" style={{ border: `1px solid red`, minHeight: "800px" }} className="App">
+				{itemList.length > 0 && itemList.map((elem, index) => {
+					return (
+						<div key={index} id={`louda`}>
+							<img src={elem.url} height="60px" width="80%" />
+							<h5>{elem.id} - {elem.title}</h5>
+						</div>
+					)
+				})}
+			</div>
+			<div
+				ref={loaderRef}
+				style={loadingCSS}
+			>
+			<span style={loadingTextCSS}>Loading...</span>
+			</div>
+		</div>
   );
 }
 
-export default App;
+export default InfiniteScrollingBitch;
---
+++ src/__App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { fetchAPI } from './fetchAPI';
+
+export class InfiniteScrollingBitch extends Component {
+	constructor() {
+    super();
+    this.state = {
+      photos: [],
+      loading: false,
+      page: 0,
+      prevY: 0
+    };
+  }
+
+	componentDidMount(){
+		this.getPhotos(this.state.page);
+
+		var options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0
+    };
+    
+    const observer = new IntersectionObserver(
+      this.handleObserver,
+      options
+    );
+    observer.observe(this.loadingRef);
+	}
+
+	handleObserver = (entities, observer) => {
+		// console.log("ss ", entities);
+    // const y = entities[0].boundingClientRect.y;
+    // if (this.state.prevY > y) {
+			// console.log("blah")
+      // const lastPhoto = this.state.photos[this.state.photos.length - 1];
+      // const curPage = lastPhoto.albumId;
+    this.getPhotos(0);
+      // this.setState({ page: curPage });
+    // }
+    // this.setState({ prevY: y });
+  }
+
+	getPhotos = (page) => {
+		this.setState({ loading: true });
+		fetchAPI()
+		.then((res) => {
+			// console.log(res)
+			this.setState({ photos: [...this.state.photos, ...res] });
+			this.setState({ loading: false });
+		})
+	}
+
+	render() {
+
+		// Additional css
+		const loadingCSS = {
+			height: "100px",
+			margin: "30px"
+		};
+
+		// To change the loading icon behavior
+    const loadingTextCSS = {
+			border: `1px solid yellow`,  
+			// display: this.state.loading ? "block" : "none"
+		};
+
+		return (
+      <div className="container">
+        <div style={{ border: `1px solid red`, minHeight: "50px" }}>
+          {this.state.photos.map((user, index) => (
+						<img src={user.url} height="100px" width="200px" />
+          ))}
+        </div>
+        <div
+          ref={loadingRef => (this.loadingRef = loadingRef)}
+          style={loadingCSS}
+        >
+          <span style={loadingTextCSS}>Loading...</span>
+        </div>
+      </div>
+		)
+	}
+}
+
+export default InfiniteScrollingBitch
